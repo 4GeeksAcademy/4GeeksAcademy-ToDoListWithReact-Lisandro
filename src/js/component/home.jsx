9991b2822c3c46842398fb14585eafebbb216d89
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2023-12-19 18:33:53 +0000

    ToDoList casi terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,14 +11,17 @@ const Home = () => {
     //Despues pones la funcion que queres que se haga cuando haces click. Se puede poner despues del evento,
     //pero es mejor que quede aca arriba asi se ve mas comodo. Es como que se usa abajo, pero se escribe arriba.
     function agregarElemento(e) {      
-        if (e.keyCode === 13) {
-            setList(list.concat(newTask));         
+        if (e.keyCode === 13) {  //agrego un condicional para que cada vez que presione la tecla enter (codigo 13) el elemento nuevo se agregue a la lista.
+            setList(list.concat(newTask)); 
+            setNewTask("") //hago que la funcion  setNewTask se actualice a 0 cada vez que apreto la tecla enter.       
         } 
     };
 
 
     function eliminar(tarea) {
-        console.log(tarea);
+       let nuevoArray = list.filter((item) => item !== tarea) //creo un nuevo array con una condicion (!== tarea)   
+        setList(nuevoArray); //llamo a la funcion nuevoArray generado para actualizar a list.
+        /* console.log(tarea); */
         //filtra todos los elementos menos al que le hacen click
         
     }
@@ -31,12 +34,12 @@ const Home = () => {
         <div className="Container">
 		
         <div className="input-group flex-nowrap">
-          <input type="text" className="form-control" onChange={(e)=>setNewTask(e.target.value)} onKeyDown={agregarElemento} placeholder="What needs to be done?" aria-label="Username" aria-describedby="addon-wrapping"/>
+          <input type="text" className="form-control" onChange={(e)=>setNewTask(e.target.value)} onKeyDown={agregarElemento} value={newTask} placeholder="What needs to be done?" aria-label="Username" aria-describedby="addon-wrapping"/>
         </div>
 
         <div className="Otros elementos de la lista">
         <ul>
-            {list.map((item) => <li>{item} <span onClick={()=>eliminar(item)}>X</span></li>)}
+            {list.map((item, index) => <li key={index}>{item} <span onClick={()=>eliminar(item)}>X</span></li>)}
         </ul>
         </div>
         </div>
